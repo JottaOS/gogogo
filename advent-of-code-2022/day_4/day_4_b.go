7560b50bc5505082ab2755f6b4d62377c80088a8
JottaOS <juanmendozasfa@gmail.com>
2024-10-16 14:48:23 -0300

    Simplificación brutal.... locura

--- advent-of-code-2022/day_4/day_4_b.go
+++ advent-of-code-2022/day_4/day_4_b.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
@@ -22,22 +20,14 @@ func main() {
 
 	for sc.Scan() {
 
-		line := sc.Text()
-		ranges := strings.Split(line, ",")
+		var x1, x2, y1, y2 int
 
-		firstElfRange := strings.Split(ranges[0], "-")
-		secondElfRange := strings.Split(ranges[1], "-")
-
-		x1, _ := strconv.Atoi(firstElfRange[0])
-		x2, _ := strconv.Atoi(firstElfRange[1])
-		y1, _ := strconv.Atoi(secondElfRange[0])
-		y2, _ := strconv.Atoi(secondElfRange[1])
+		fmt.Sscanf(sc.Text(), "%d-%d,%d-%d", &x1, &x2, &y1, &y2)
 
 		// check if first range overlaps at least one section with second range or vice versa
-		if (x1 >= y1 && x1 <= y2) || (x2 >= y1 && x2 <= y2) || (y1 >= x1 && y1 <= x2) || (y2 >= x1 && y2 <= x2) {
+		if x1 <= y2 && x2 >= y1 {
 			overlaps++
 		}
-
 	}
 
 	fmt.Println(overlaps)
